667541b51eb2ed213090ba1513dc53db7101200b
annie.wu <annie.wu@blackshark.com>
2021-01-12 18:59:13 +0800

    python: date.py: add some print info

--- python/date.py
+++ python/date.py
@@ -1,12 +1,9 @@
 # coding:utf-8
 
-'''
-使用中文需要注意编码格式：
-SyntaxError: Non-ASCII character
-'''
 import datetime
 
-DATE_FORMAT = "%Y-%m-%d"  # date to string format
+DATE_FORMAT = "%Y-%m-%d"  # date to string format 设置日期格式
+ODVB_BASE_PATTERN = "/dailybuild/odvb/%s/*"
 
 today = datetime.date.today()
 print today
@@ -16,8 +13,11 @@ print before_1xday_ago
 
 folder = "2020-12-08_sm8350_debug_ssd_20201124"
 imagedate_L = folder.split("_")
+print imagedate_L
 
 imgdt = datetime.datetime.strptime(imagedate_L[0], DATE_FORMAT)
+print imgdt
 
 imagedate = datetime.date(imgdt.year, imgdt.month, imgdt.day)
 print imagedate
+
