c7f620bfaa6bb938de17cb34803a35a5d7235dc3
annie.wu <annie.wu@blackshark.com>
2021-01-13 18:41:43 +0800

    groovy: hello.groovy:  add format the string

--- groovy/hello.groovy
+++ groovy/hello.groovy
@@ -1,1 +1,14 @@
 println("Hello World!")
+
+def get_label() {
+    def label = String.format("%02d",1)//格式化 字符串，不足2位的 前面补齐 0
+    println "the new label is ${label}"
+}
+
+def example2() {
+    println 'Hello from example2'
+}
+get_label()
+example2()
+
+return this
