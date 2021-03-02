0c0011d3c6239df2d88dd5eb5e6ba51e4fc9d0c2
annie.wu <annie.wu@blackshark.com>
2021-03-02 16:03:59 +0800

    shell: log.sh: add colorful log printf info file

---
+++ shell/log.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo -e "\e[32m[ info `date "+%Y-%m-%d %H:%M:%S"`]\e[0m"
+
+echo -e "\e[31m[ error `date "+%Y-%m-%d %H:%M:%S"`]\e[0m"
+
+echo -e "\e[33m[ warning `date "+%Y-%m-%d %H:%M:%S"`]\e[0m"
